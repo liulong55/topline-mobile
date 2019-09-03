dfe3dc6e9bc22d0a959fcb51785a0bde87fb5456
liulong <1362457117@qq.com>
2019-09-03 22:04:32 +0800

    本地化（提示文字汉化）

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router/index'
 import store from './store/index'
 
 import Vant from 'vant' // 导入组件库
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN' // 导入中文包
 import 'vant/lib/index.css'
 import '../src/styles/index.less'
 Vue.use(Vant)
 Vue.use(VeeValidate)// 配置插件VeeValidate
+Validator.localize('zh_CN', zhCN) // 配置中文
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -78,5 +78,4 @@ export default {
         width: 100%;
     }
 }
-
 </style>
