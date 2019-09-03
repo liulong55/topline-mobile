cb431cf705eea722d5d3be4e578957b04179ccaa
liulong <1362457117@qq.com>
2019-09-03 19:10:20 +0800

    登录界面-页面布局

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 
-import Vant from 'vant'
+import Vant from 'vant' // 导入组件库
 import 'vant/lib/index.css'
+import '../src/styles/index.less'
 Vue.use(Vant)
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,18 @@
 <template>
-     <div>
-         aaa
-     </div>
+  <div>
+    <!-- 导航头 -->
+    <van-nav-bar title="登录" />
+    <!-- 俩输入框 -->
+    <van-cell-group>
+      <van-field v-model="value" placeholder="请输入手机号" left-icon="phone-o" />
+      <van-field v-model="value" placeholder="请输入验证码" left-icon="star-o">
+          <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+      <div class="login-btn">
+        <van-button class="btn" type="info">登录</van-button>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +22,10 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+.login-btn{
+    padding: 20px;
+    .btn{
+        width: 100%;
+    }
+}
 </style>
