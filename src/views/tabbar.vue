01ab7aacf67dc1e70d70a8de3aa6c8c8391ef296
liulong <1362457117@qq.com>
2019-09-04 10:46:07 +0800

    tabbar组件

--- src/views/tabbar.vue
+++ src/views/tabbar.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <router-view></router-view>
-    这是切换栏
+<van-tabbar route>
+  <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+  <van-tabbar-item to="/bq" icon="search">问答</van-tabbar-item>
+  <van-tabbar-item to="/sp" icon="friends-o">视频</van-tabbar-item>
+  <van-tabbar-item to="/wd" icon="setting-o">我的</van-tabbar-item>
+</van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'tabbar'
 }
 </script>
 
