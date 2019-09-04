b0ba175684f818be6ab4c6ee30dc8405cb67950e
liulong <1362457117@qq.com>
2019-09-04 10:52:20 +0800

    首页-布局1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,19 @@
 <template>
-   <div>
-       home
-   </div>
+  <div>
+    <!-- 导航头 -->
+    <van-nav-bar title="首页" />
+    <!-- 频道列表 -->
+    <van-tabs animated>
+  <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
+    内容 {{ index }}
+  </van-tab>
+</van-tabs>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
