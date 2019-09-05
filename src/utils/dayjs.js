4c6b1b2f60a3f8ae23f5ae1469e6e5d4a67ce3b1
liulong <1362457117@qq.com>
2019-09-05 22:37:47 +0800

    dayjs汉化

--- src/utils/dayjs.js
+++ src/utils/dayjs.js
@@ -1,7 +1,9 @@
 // 导入时间包
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
-
+// 导入语言包
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
 dayjs.extend(relativeTime)
 
 // 获取相对时间的方法
@@ -9,5 +11,6 @@ export const fmtDate = (value) => {
   // console.log(dayjs())
   // dayjs() 获取当前时间（dayjs中一个特殊的对象）
   // dayjs(value) 指定的时间（dayjs中一个特殊的对象）
-  return dayjs().from(dayjs(value))
+//   return dayjs().from(dayjs(value))
+  return dayjs().to(dayjs(value))
 }
