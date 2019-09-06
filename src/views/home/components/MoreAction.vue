d2166445b7a6de36c2658980888f44873fb79369
liulong <1362457117@qq.com>
2019-09-06 14:13:32 +0800

    弹出层显示的内容

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -1,7 +1,18 @@
 <template>
   <van-dialog v-model="show" :showConfirmButton="false"
     closeOnClickOverlay>
-        HAHA
+    <van-cell-grounp v-show="!showReports">
+        <van-cell title="不感兴趣"></van-cell>
+         <van-cell title="反感垃圾内容" is-link @click='showReports=true'></van-cell>
+          <van-cell title="拉黑作者"></van-cell>
+    </van-cell-grounp>
+    <!-- 举报文章 -->
+      <van-cell-grounp v-show="showReports">
+          <van-cell  icon="arrow-left" @click="showReports=false"></van-cell>
+        <van-cell title="标题夸张"></van-cell>
+         <van-cell title="低俗色情"></van-cell>
+          <van-cell title="错别字多"></van-cell>
+    </van-cell-grounp>
   </van-dialog>
 </template>
 
@@ -10,7 +21,8 @@ export default {
   name: 'MoreAction',
   data () {
     return {
-      show: true
+      show: true,
+      showReports: false
     }
   }
 }
