9d0d1bdcd7c072f5bd4f3e13051e7983606d4f3c
liulong <1362457117@qq.com>
2019-09-07 14:29:00 +0800

    举报文章-优化

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -9,15 +9,18 @@
     <!-- 举报文章 -->
       <van-cell-group v-show="showReports">
           <van-cell  icon="arrow-left" @click="showReports=false"/>
-        <van-cell title="标题夸张"/>
-         <van-cell title="低俗色情"/>
-          <van-cell title="错别字多"/>
+          <van-cell
+           v-for="item in reportList"
+            :key="item.type"
+            :title="item.title"
+            @click="handle('report',item.type)"
+            />
     </van-cell-group>
   </van-dialog>
 </template>
 
 <script>
-import { dislikeArticle } from '../../../api/article'
+import { dislikeArticle, reportArticle } from '../../../api/article'
 import { blacklists } from '../../../api/user'
 export default {
   name: 'MoreAction',
@@ -34,21 +37,37 @@ export default {
   },
   data () {
     return {
-      showReports: false
+      showReports: false,
+      // 举报类型： 0-其他问题，1-标题夸张，2-低俗色情，3-错别字多，4-旧闻重复，5-广告软文，6-内容不实，7-涉嫌违法犯罪，8-侵权'
+      reportList: [
+        { title: '标题夸张', type: 1 },
+        { title: '低俗色情', type: 2 },
+        { title: '错别字多', type: 3 },
+        { title: '旧闻重复', type: 4 },
+        { title: '广告软文', type: 5 },
+        { title: '内容不实', type: 6 },
+        { title: '涉嫌违法犯罪', type: 7 },
+        { title: '侵权', type: 8 },
+        { title: '其他问题', type: 0 }
+      ]
     }
   },
   methods: {
     // 点击所有cell的时候,都执行方法
     // 通过type判断具体要执行的操作
-    handle (type) {
+    handle (type, reportType) {
       switch (type) {
         case 'dislike':
           // 不感兴趣
           this.dislike()
           break
+          // 拉黑作者
         case 'blacklist':
           this.blacklistUser()
           break
+          // 举报文章
+        case 'report':
+          this.report(reportType)
       }
     },
     // 不感兴趣
@@ -69,6 +88,20 @@ export default {
         // 通知父组件,隐藏弹出框,删掉当前作者的文章
         this.$toast.success('操作成功')
         this.$emit('handleSuccess')
+      } catch (error) {
+        this.$toast.fail('操作失败')
+      }
+    },
+    // 举报文章
+    async report (reportType) {
+      try {
+        await reportArticle({
+          target: this.article.art_id,
+          type: reportType
+        })
+        // 告诉父组件隐藏对话框
+        this.$emit('input', false)
+        this.$toast.success('操作成功')
       } catch (error) {
         this.$toast.fail('操作失败')
       }
