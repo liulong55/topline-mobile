3b632b1d5704b622ba6d20bcc10f3620e8f320fb
liulong <1362457117@qq.com>
2019-09-07 19:22:29 +0800

    频道管理-展示推荐频道

--- src/api/channel.js
+++ src/api/channel.js
@@ -12,3 +12,7 @@ export const getchannels = () => {
     url: '/app/v1_0/user/channels'
   })
 }
+// 获取所有频道
+export const getAllChannels = () => {
+  return request.get('/app/v1_0/channels')
+}
--- src/views/home/components/ChannelEdit.vue
+++ src/views/home/components/ChannelEdit.vue
@@ -24,6 +24,7 @@
       >完成</van-button>
     </van-cell>
     <van-grid>
+        <!-- 我的频道 -->
       <van-grid-item
         v-for="channel in channels"
         :key="channel.id"
@@ -42,15 +43,16 @@
     <van-cell title="推荐频道" label="点击添加频道" />
     <van-grid>
       <van-grid-item
-        v-for="value in 8"
-        :key="value"
-        text="文字"
+        v-for="item in recommendChannels"
+        :key="item.id"
+        :text="item.name"
       />
     </van-grid>
 </van-popup>
 </template>
 
 <script>
+import { getAllChannels } from '../../../api/channel'
 export default {
   name: 'ChannelEdit',
   props: {
@@ -58,15 +60,50 @@ export default {
       type: Boolean,
       required: true
     },
+    // 接收父组件传过来的我的频道
     channels: {
       type: Array,
       required: true
     }
   },
+  computed: {
+    // 推荐频道
+    recommendChannels () {
+      // 1. 获取我的频道中所有id组成的数组
+      // map() 遍历数组，返回一个新的数组，新数组中的元素由回调函数中返回的元素组成
+      const ids = this.channels.map((channel) => {
+        return channel.id
+      })
+      // 2.过滤所有频道,把频道id出现在上面数组中的项去除
+      // filter(),把满足条件的item,返回组成一个新的数组
+      return this.allChannels.filter((channel) => {
+        // includes()   es6新增，判断数组中是否包含某一项
+        return !ids.includes(channel.id)
+      })
+    }
+  },
   data () {
     return {
-      isEdit: false
+      // 是否是编辑模式
+      isEdit: false,
+      // 存储所有频道
+      allChannels: []
     }
+  },
+  methods: {
+    // 加载所有的频道列表
+    async loadAllChannels () {
+      try {
+        const data = await getAllChannels()
+        this.allChannels = data.channels
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  created () {
+    // 加载所有的频道列表
+    this.loadAllChannels()
   }
 }
 </script>
