bc2272a796aa6c75fd29f6e4fcf8cd015fe30b0d
liulong <1362457117@qq.com>
2019-09-09 19:20:32 +0800

    文章详情-点赞和取消点赞

--- src/views/detail/component/MoreAction.vue
+++ src/views/detail/component/MoreAction.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="more-action">
+        <!-- 用户对文章的态度,-1无态度,0不喜欢,1点赞 -->
     <van-button
-      icon="star-o"
+      :icon="article.attitude===1? 'star':'star-o'"
       round
       :loading="false"
       type="danger"
+      @click="handleLike"
     >点赞</van-button>
 
     <van-button
@@ -17,8 +19,30 @@
 </template>
 
 <script>
+import { likeArticle, unLikeArticle } from '../../../api/article'
 export default {
+  props: ['article'], // 从主页带过来的
+  methods: {
+    // 点赞和取消点赞
+    async handleLike () {
+      // 判断是否登录
 
+      try {
+        // 点赞或者取消点赞
+        if (this.article.attitude === 1) {
+          // 当前已经点赞,取消点赞
+          await unLikeArticle(this.article.art_id)
+          this.article.attitude = -1
+        } else {
+          // 1是无态度
+          await likeArticle(this.article.art_id)
+          this.article.attitude = 1
+        }
+      } catch (error) {
+        this.$toast.fail('操作失败')
+      }
+    }
+  }
 }
 </script>
 
