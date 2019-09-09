4ac2893c0e08c73dae6e419ce0240d0b8d277cc6
liulong <1362457117@qq.com>
2019-09-09 20:40:55 +0800

    keep-alive,解决了进详情页出来重新加载的问题

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- <keep-alive> 包裹动态组件时，会缓存不活动的组件实例，而不是销毁它们 -->
+      <!-- include - 字符串或正则表达式。只有名称匹配的组件会被缓存。
+exclude - 字符串或正则表达式。任何名称匹配的组件都不会被缓存。
+max - 数字。最多可以缓存多少组件实例。 -->
+    <keep-alive exclude="Detail">
+     <router-view/>
+    </keep-alive>
   </div>
 </template>
 
